b5364430f23a90de127487b0662392053e5c41d2
chintan <chintan@gmail.com>
2026-04-11 10:54:40 +0530

    all

---
+++ 003_logical/P1.py
@@ -0,0 +1,110 @@
+# *****
+# *****
+# *****
+# *****
+
+# lines = 3
+# for j in range(lines):
+#     for i in range(lines):
+#         print("*",end="")
+#     print()
+
+# for i in range(5):
+#     print(5*"*")
+
+# *
+# **
+# ***
+# ****
+# *****
+
+
+# lines = 5
+# for j in range(lines):
+#     for i in range(j+1):
+#         print("*",end="")
+#     print()
+
+# *****
+# ****
+# ***
+# **
+# *
+
+# lines = 5
+# for j in range(lines):
+#     for i in range(lines-j):
+#         print("*",end="")
+#     print()
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+# lines = 5
+# for j in range(lines):
+#     for k in range(lines-(j+1)):
+#         print(" ",end="")
+#     for i in range(j+1):
+#         print("*",end="")
+#     print()
+
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+
+# lines = 5
+# for j in range(lines):
+#     for k in range(j):
+#         print(" ",end="")
+#     for i in range(lines-j):
+#         print("*",end="")
+#     print()
+
+#    *
+#   * *
+#  * * *
+# * * * *
+
+
+# lines = 5
+# for j in range(lines):
+#     for k in range(lines-(j+1)):
+#         print(" ",end="")
+#     for i in range(j+1):
+#         print("* ",end="")
+#     print()
+
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+
+
+# lines = 5
+# for j in range(lines):
+#     for k in range(lines-(j+1)):
+#         print(" ",end="")
+#     for i in range((j*2)+1):
+#         print("*",end="")
+#     print()
+
+    #   *
+    #  * *
+    # * * *
+    #  * *
+    #   *
+
+
+# lines = 5
+# for j in range(lines):  
+#     print(" "*(lines-(j+1)),"*"*(j+1))
+ 
+    

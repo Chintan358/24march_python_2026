2bb44f1b1de1ed4cf0ed35a38fad6b8a39c0803c
chintan <chintan@gmail.com>
2026-06-16 13:46:05 +0530

    all

--- 015_django/myapp/models.py
+++ 015_django/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.CharField(max_length=50)
+    age = models.IntegerField()
+    phone = models.CharField(max_length=20,null=True)
+    dept = models.CharField(max_length=15,null=True)
+    subject = models.CharField(max_length=20,default='Python')
+    
--- 016_TemplateIngegration/myapp/admin.py
+++ 016_TemplateIngegration/myapp/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from myapp.models import *
+
 # Register your models here.
-admin.site.register(Student)

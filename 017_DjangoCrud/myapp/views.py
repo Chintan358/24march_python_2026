1c0814171a427c23988e75b0bb08c0e7f79e5077
chintan <chintan@gmail.com>
2026-06-23 12:24:28 +0530

    all

--- 017_DjangoCrud/myapp/views.py
+++ 017_DjangoCrud/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from myapp.models import *
 # Create your views here.
 def index(request):
@@ -7,11 +7,35 @@ def index(request):
 def add_student(request):
     if request.method=='POST':
         data = request.POST
+        id = data.get('id')
         name = data.get('name')
         email = data.get("email")
         age = data.get("age")
 
-        Student.objects.create(name=name,email=email,age=age)
-        return render(request,"index.html",{"msg":"Registration successfully !!!"})
+        if id:
+            student = Student.objects.get(id=id)
+            student.name = name
+            student.email = email
+            student.age = age
+            student.save()
+            return render(request,"index.html",{"msg":"Update successfully !!!"})
+        else:
+            Student.objects.create(name=name,email=email,age=age)
+            return render(request,"index.html",{"msg":"Registration successfully !!!"})
         
     return render(request,"index.html")
+
+def display(request):
+    students = Student.objects.all()
+    return render(request,"display.html",{"students":students})
+
+def delete_student(request):
+    id = request.GET.get("id")
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect("display")
+
+def retrive(request):
+    id = request.GET.get("id")
+    student= Student.objects.get(id=id)
+    return render(request,"index.html",{"student":student})

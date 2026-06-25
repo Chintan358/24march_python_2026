551df8cda1d9fce18bf752e8c2dc772e520fdf77
chintan <chintan@gmail.com>
2026-06-25 12:27:11 +0530

    all

---
+++ 018_Django_Relation/myapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from myapp.models import *
+
+
+admin.site.register(Person)
+admin.site.register(Passport)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Book)
+admin.site.register(Author)
